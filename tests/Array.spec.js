31a2f5be19cf03dbdb85a458090cd64d15710450
matan <matanbsn@gmail.com>
2024-07-17 22:48:45 +0300

    1)first ajustments for the projact 2)a dummy test

---
+++ tests/Array.spec.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+describe('dummyTest', () => {
+    it('should check array if values equal to ', () => { 
+
+        const names = ['yarden','matan','shiraz']
+        expect(names.sort()).to.be.eql(['matan','shiraz','yarden'])    
+     })
+      
+    });
+
