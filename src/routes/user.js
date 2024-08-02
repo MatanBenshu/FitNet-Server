1546da667d56b4b1792cfdf87eddd3a1a7a5dad6
reamlevi <reamlevi@gmail.com>
2024-08-02 20:04:53 +0300

    Add user Api

---
+++ src/routes/user.js
@@ -0,0 +1,121 @@
+import express from 'express';
+const router = express.Router();
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+//update user
+router.put('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id ) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            
+            res.status(200).json('Account has been updated');
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json('You can update only your account!');
+    }
+});
+
+//delete user
+router.delete('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id ) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json('Account has been deleted');
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json('You can delete only your account!');
+    }
+});
+
+//get a user
+router.get('/', async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+        const user = userId
+            ? await User.findById(userId)
+            : await User.findOne({ username: username });
+        // eslint-disable-next-line no-unused-vars
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//get friends
+router.get('/friends/:userId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const friends = await Promise.all(
+            user.followings.map((friendId) => {
+                return User.findById(friendId);
+            })
+        );
+        let friendList = [];
+        friends.map((friend) => {
+            const { _id, username, profilePicture } = friend;
+            friendList.push({ _id, username, profilePicture });
+        });
+        res.status(200).json(friendList);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//follow a user
+
+router.put('/:id/follow', async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            if (!user.followings.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followings: req.body.userId } });
+                res.status(200).json('user has been followed');
+            } else {
+                res.status(403).json('you already follow this user');
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json('you cant follow yourself');
+    }
+});
+
+//unfollow a user
+
+router.put('/:id/unfollow', async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            if (user.followings.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followings: req.body.userId } });
+                res.status(200).json('user has been unfollowed');
+            } else {
+                res.status(403).json('you don\'t follow this user');
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json('you cant unfollow yourself');
+    }
+});
+
+export default router;
