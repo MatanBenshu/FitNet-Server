9a74857cf57e4af32aa412ed067e91931213a0f1
reamlevi <reamlevi@gmail.com>
2024-08-02 20:00:00 +0300

    Add Event schema

---
+++ src/models/Event.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const EventSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String, 
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: Date,
+            required: true,
+        },
+        startTime: {
+            type: String,
+            required: true,
+        },
+        desc: {
+            type: String,
+            max: 500,
+        },
+        img: {
+            type: String,
+        },
+        group: {
+            type: String,
+            default: '',
+        },
+        attendees: { 
+            type: Array,
+            default:function() {
+                return [this.userId];
+            },
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Event', EventSchema);
